c3a6f9521c4a9044a0b65537e3fe549a07a23d1c
Wagner Terry <goncalveswagner15@gmail.com>
2023-08-15 17:56:28 -0300

    test: Teste para incluir usuário

--- test/user.spec.js
+++ test/user.spec.js
@@ -31,4 +31,15 @@ describe('Testes usando o banco mysql', () => {
 
         expect(response.statusCode).toEqual(200)
     })
+    it('Deve inserir um novo usuário com sucesso', async () => {
+        const response = await request(app)
+            .post('/users')
+            .send({
+                id: 1,
+                nome: 'Joao'
+            })
+        expect(response.statusCode).toEqual(201);
+        expect(response.body.success).toBe(true);
+        expect(response.body.message).toBe('Usuario adicionado com sucesso')
+    })
 })
