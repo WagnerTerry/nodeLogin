fc05113d6427a87b40778e95011b4246b5141ad0
Wagner Terry <goncalveswagner15@gmail.com>
2023-09-06 16:51:44 -0300

    feat: criando rota para buscar um usuário por id

--- app.js
+++ app.js
@@ -1,124 +1,152 @@
-require('dotenv').config();
-const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const db = process.env.DB_ENV === "test" ? require('./src/models/dbConfig.test') : require('./src/models/dbConfig')
+const db =
+  process.env.DB_ENV === "test"
+    ? require("./src/models/dbConfig.test")
+    : require("./src/models/dbConfig");
 
 const app = express();
 
 // Configuração de middleware
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({
-    secret: 'segredo',
+app.use(
+  session({
+    secret: "segredo",
     resave: false,
-    saveUninitialized: true
-}))
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'src', 'views'));
+    saveUninitialized: true,
+  })
+);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src", "views"));
 
 // Rotas View
-app.get('/', (req, res) => {
-    res.render('login', {title: "teste"})
-})
+app.get("/", (req, res) => {
+  res.render("login", { title: "teste" });
+});
 
-app.get('/home', (req, res) => {
-    if(req.session.loggedIn){
-        res.render('home')
-    } else {
-        res.redirect('/')
-    }
-})
+app.get("/home", (req, res) => {
+  if (req.session.loggedIn) {
+    res.render("home");
+  } else {
+    res.redirect("/");
+  }
+});
 
-app.get('/logout', (req, res) => {
-    req.session.loggedIn = false
-    res.redirect('/')
+app.get("/logout", (req, res) => {
+  req.session.loggedIn = false;
+  res.redirect("/");
+});
 
-})
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
 
-app.post('/login', (req, res) => {
-    const {username, password} = req.body;
-    
-    if(username === 'usuario' && password === '123'){
-        req.session.loggedIn = true
-        // res.status(200).json({success: true, message: "Login realizado com sucesso"})
-         return res.redirect('/home')
-
-    } else {
-        return res.status(401).json({success: false, message: 'Login inválido'})
-    }
-})
+  if (username === "usuario" && password === "123") {
+    req.session.loggedIn = true;
+    // res.status(200).json({success: true, message: "Login realizado com sucesso"})
+    return res.redirect("/home");
+  } else {
+    return res.status(401).json({ success: false, message: "Login inválido" });
+  }
+});
 
 // Rotas banco de dados
-app.get('/users', (req, res) => {
-    db.query('select * from users', (err, results) => {
-        if(err){
-            console.error('erro ao executar consulta: ', err);
-            res.status(500).send('Erro no servidor')
-            return
-        }
-        res.json(results)
+app.get("/users", (req, res) => {
+  db.query("select * from users", (err, results) => {
+    if (err) {
+      console.error("erro ao executar consulta: ", err);
+      res.status(500).send("Erro no servidor");
+      return;
+    }
+    res.json(results);
+  });
+});
 
-    })
-})
+app.get("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
 
-app.post('/users', (req, res) => {
-    const {nome} = req.body
+  db.query("select * from users where id = ?", [id], (err, results) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ success: false, error: "Erro interno no servidor" });
+    }
 
-    db.query('insert into users (nome) values (?)',
-     [nome],
-     (err, results) => {
-        if(err){
-            res.status(500).json({success: false, error: err});
-            return;
-        }
-        res.status(201).json({success: true, message: "Usuario adicionado com sucesso", id: results.insertId})
-    })
-})
+    if (results.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Usuário não encontrado" });
+    }
 
-app.put('/users/:id', (req, res) => {
-    const id = req.params.id
-    const nome = req.body
+    return res.status(200).json({ success: true, data: results[0] });
+  });
+});
 
+app.post("/users", (req, res) => {
+  const { nome } = req.body;
 
-    db.query('update users set nome = ? where id = ?', 
-    
+  db.query("insert into users (nome) values (?)", [nome], (err, results) => {
+    if (err) {
+      res.status(500).json({ success: false, error: err });
+      return;
+    }
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Usuario adicionado com sucesso",
+        id: results.insertId,
+      });
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const nome = req.body;
+
+  db.query(
+    "update users set nome = ? where id = ?",
+
     [nome, id],
     (err, results) => {
+      if (err) {
+        return res.status(500).json({ success: false, error: err });
+      }
 
-        if(err){
-            return res.status(500).json({success: false, error: err})
-        }
+      if (results.affectedRows === 0) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Usuário não encontrado" });
+      }
 
-        if(results.affectedRows === 0){
-            return res.status(404).json({success: false, message: "Usuário não encontrado"})
-        }
-
-        return res.status(200).json({success: true, message: 'Usuário atualizado'})
+      return res
+        .status(200)
+        .json({ success: true, message: "Usuário atualizado" });
     }
-    )
-})
+  );
+});
 
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
 
-    db.query('delete from users where id = ?', 
-    [id],
-    (err, results) => {
-        if(err){
-            return res.status(500).json({sucess: false, error: err})
-        }
-
-        if(results.affectedRows === 0){
-            return res.status(404).json({success: false, error: "Usuário não encontrado"})
-        }
-        return res.status(204).send()
+  db.query("delete from users where id = ?", [id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ sucess: false, error: err });
     }
-    )
-})
 
-module.exports = app
+    if (results.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Usuário não encontrado" });
+    }
+    return res.status(204).send();
+  });
+});
+
+module.exports = app;
 
 //app.listen(3000, () => console.log("servidor na porta 3000"))
